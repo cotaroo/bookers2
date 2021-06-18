911d94a9ec5fc2ec2dfc60764d733a19d7488d3e
cotaroo <cotaro.50tal@gmail.com>
2021-06-18 00:23:40 +0000

    #5 update header layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  get 'home/about' => 'homes#about', as: :home_about
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 end
