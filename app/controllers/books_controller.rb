f82876bbb0cf648380ba7e6d8d2f4f550a736a7b
cotaroo <cotaro.50tal@gmail.com>
2021-06-20 22:28:43 +0000

    #22 add feature create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,6 @@
 class BooksController < ApplicationController
+  
   def new
-  end
-
-  def create
   end
 
   def index
@@ -10,10 +8,24 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
 
   def show
   end
 
   def destroy
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
