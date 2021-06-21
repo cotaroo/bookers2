2bb3600b936c3dffb8e6a670891e093f835aa68f
cotaroo <cotaro.50tal@gmail.com>
2021-06-21 23:17:03 +0000

    #36 add book edit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about', as: :home_about
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
   resources :users, only: [:show, :index]
 end
