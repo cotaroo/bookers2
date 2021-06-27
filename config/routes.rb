c7dc47b92d5cf66a583e6be849de91a82cc656f2
cotaroo <cotaro.50tal@gmail.com>
2021-06-27 23:21:50 +0000

    #65 add feature favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about', as: :home_about
-  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :index, :edit, :update]
 end
