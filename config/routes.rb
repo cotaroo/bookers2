f9cb306edfa150174f1a3a54ad2a38ff2ed0eb79
cotaroo <cotaro.50tal@gmail.com>
2021-06-28 07:44:21 +0000

    #69 add feature comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: :home_about
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :index, :edit, :update]
 end
